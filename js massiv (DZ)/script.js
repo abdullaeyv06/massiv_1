f9fe1b9bf6fc578450825584e581c4e8bfc5e076
abdullaeyv06 <145132841+abdullaeyv06@users.noreply.github.com>
2023-11-24 16:00:48 +0500

    Add files via upload

---
+++ js massiv (DZ)/script.js
@@ -0,0 +1,45 @@
+let names = ["Radmir" , "Abdumanon" , "Daler" , "Dimon"]
+
+let even = []
+let odd = []
+
+names.filter(item => {
+    if (item.length %2 === 0) {
+        names.push(even)
+    } else {
+      names.push(odd)
+    }
+})
+
+
+
+
+
+
+// if (names.length %2 === 0) {
+//     even = names
+// } else {
+//     odd = names
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let array = [3,5,8,2,9, 11, 44, 66,6]
+
+// let all = 0
+
+// array.sort((a,b) => a - b)
+
+// array.filter((item) => all += item)
+
+
+// console.log(all);
+
